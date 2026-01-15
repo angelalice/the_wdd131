89f28003cd4cd33223bd2802e464fb67bd84c181
angelalice <71465499+angelalice@users.noreply.github.com>
2026-01-15 15:42:46 -0700

    updates

---
+++ week-2/ponder/prove/dom-theme.js
@@ -0,0 +1,30 @@
+
+let selectElem = document.querySelector('#theme-select');
+let pageContent = document.querySelector('body');
+let imgChange = document.getElementById("logo");
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current === 'ocean') {
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/ocean.jpg')";
+        pageContent.style.fontFamily = "Papyrus, fantasy";
+        pageContent.style.color= "rgb(231, 41, 73)";
+        pageContent.style.fontSize = "2em";
+        imgChange.style.width = "20%";
+        imgChange.style.opacity = "0.5";
+    } else if (current === 'forest') {
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/forest.jpg')";
+        pageContent.style.fontFamily = "Impact, sans-serif";
+        
+    } else if (current === 'desert') {
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/desert.jpg')";
+        pageContent.style.fontFamily = "'Big Caslon', serif";
+    } else {
+        // default
+        document.body.style.backgroundImage = "none";
+        pageContent.style.fontFamily = "Georgia, serif";
+    }
+}
+          
