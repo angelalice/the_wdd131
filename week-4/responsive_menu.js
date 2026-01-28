cc61367f8a96a9a8b748e0ac2b059b7a17cf8887
angelalice <71465499+angelalice@users.noreply.github.com>
2026-01-27 19:15:29 -0700

    made changes

---
+++ week-4/responsive_menu.js
@@ -0,0 +1,9 @@
+const nav_link = document.querySelector('.active');
+const btn = document.querySelector('.menu-btn');
+
+btn.addEventListener('click',() =>{
+    nav_link.classList.toggle('active');
+    
+});
+
+
