c27545853a4e109757c62b9118ca4338fc6c57af
angelalice <71465499+angelalice@users.noreply.github.com>
2026-01-30 09:11:19 -0700

    Update coolpics.js

--- week-3/coolpics/coolpics.js
+++ week-3/coolpics/coolpics.js
@@ -1,6 +1,35 @@
 const nav = document.querySelector('.active');
 const showList = document.querySelector('#standby');
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeBtn = modal.querySelector('.close-viewer');
 
 showList.addEventListener('click',() =>{
     nav.classList.toggle('active');
+}); 
+
+//Opens the modal when an image is clicked
+gallery.addEventListener('click',(e) => {
+    if (e.target.tagName == 'IMG'){
+        modalImage.src = e.target.src; 
+        modal.showModal();
+        console.log(modalImage);
+    }   
 });
+
+
+//Closes the modal 
+closeBtn.addEventListener('click', ()=>{
+    modal.close();
+})
+
+//Closes modal when area around the image is clicked
+modal.addEventListener('click', (e) => {
+    if(e.target == modal){
+        modal.close();
+    }
+    
+})
+
+
