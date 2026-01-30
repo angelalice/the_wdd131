c22375c5bdbd8f3a2d350c251b7bf1a2a7ea6c02
angelalice <71465499+angelalice@users.noreply.github.com>
2026-01-30 07:41:50 -0700

    Update coolpics.js

--- week-3/coolpics/coolpics.js
+++ week-3/coolpics/coolpics.js
@@ -4,16 +4,3 @@ const showList = document.querySelector('#standby');
 showList.addEventListener('click',() =>{
     nav.classList.toggle('active');
 });
-
-/* 
-const btn = document.querySelector('.menu-btn');
-
-btn.addEventlistener('click',function()){
-    btn.classList.toggle('.active');
-
-}
- add border: 1px solid;
- (0, 8px)
-    transition: transform 0.7s ease-in-out;
-
-*/
