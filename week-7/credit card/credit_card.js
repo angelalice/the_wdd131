dea44cc48eece964133181560e2878b7666c35a2
angelalice <71465499+angelalice@users.noreply.github.com>
2026-02-21 15:31:37 -0700

    finished assignment

---
+++ week-7/credit card/credit_card.js
@@ -0,0 +1,61 @@
+
+const theForm = document.querySelector('#checkoutForm');
+const creditCardContainer = document.querySelector('#creditCardNumberContainer');
+
+//validations and errors
+function displayError(msg) {
+	// display error message
+	document.querySelector('.errors').textContent = msg
+}
+
+function isCardNumberValid(number) {
+	// normally we would contact a credit card service...but we don't know how to do that yet. So to keep things simple we will only accept one number
+	return number === '1234123412341234'
+}
+
+function submitHandler(event) {
+	event.preventDefault();
+  let errorMsg = '';
+	displayError('');
+
+  let cardNumber = document.querySelector('#creditCardNumber');
+  const cardNum = cardNumber.value.trim();
+    // Check if it's numeric and valid in one go
+    /* /^([4-7] 5 | [1-3]0)\d{14}$/ */
+      if (!/^\d{16}$/.test(cardNum)) {
+      errorMsg += 'Card number must be 16 digits\n';
+      } else if (!isCardNumberValid(cardNum)) {
+        errorMsg += 'Card number is not valid\n';
+      }
+    
+    //check date
+    const expYear = Number(document.querySelector('#year').value);   
+    const expMonth = Number(document.querySelector('#month').value);
+    const currentDate = new Date()
+
+    if (2000 + expYear < currentDate.getFullYear() || (2000 + expYear === currentDate.getFullYear() && expMonth <= (currentDate.getMonth() + 1))
+    ) {
+        errorMsg += 'Card is expired\n';
+    }
+
+    let cvc = document.querySelector('#cvc');
+    const cvv = cvc.value.trim();
+    //check cvc
+    if (!/^\d{3}$/.test(cvv)) {
+      errorMsg += 'CVC/CVV must be 3 digits\n';
+    }
+  
+
+
+    if (errorMsg !== '') {
+		// there was an error. stop the form and display the errors.
+		displayError(errorMsg)
+		return;
+    }
+    // Success: show a confirmation message
+    const formContainer = document.getElementById('checkoutForm');
+    formContainer.innerHTML = '<h2>Thank you for your purchase.</h2>';
+}
+  
+document.querySelector('#checkoutForm').addEventListener('submit', submitHandler)
+          
